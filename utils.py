8d28cebed310f681b1ac20abad1ee014c06f8ac1
erlebach <gordon.erlebach@gmail.com>
2022-05-17 14:52:12 -0400

    First commit.

---
+++ utils.py
@@ -0,0 +1,174 @@
+import pandas as pd
+import date_library as datelib
+
+def clean_dataframe(df0):
+    df = df0.copy()
+    df = seg_and_true_od_dest_match(df)
+    df = keep_only(df, "OPERATING_COMPANY", "CM")
+    # Restrict df to itineraries operated by Copa
+    # Holding_OD is the section of the itinerary managed by Copa
+    df = df[df['TRUE_OD'] == df['HOLDING_OD']]
+
+    # Is the order of the next two lines important? Should not be.
+    df = PNRs_with_multiple_MEMBER_IDs(df)
+    df = keep_depart_country_is_addr_country(df)
+
+    # Remove itineraries that originate and terminate in the same country
+    # There is no way to determine the country of origin of these flights
+    df = remove_flights_within_country(df)
+
+    # Compute the number of each party and add a new column "PARTYSZ"
+
+    # Add a column equal to the number of tickets on this PNR
+    df = party_size(df)
+    # Each triplet (PNR / FlIGHT# / FLIGHT_DATE) should only appear once
+    df = one_record_per_family(df)
+
+    df.fillna(-1, inplace=True)
+    float_cols = df.columns[df.dtypes == 'float']
+    df[float_cols] = df[float_cols].astype('int')   # recommended use of view generates error
+    # It would nice to identify the dates automatically
+    date_cols = ['ENROLL_DATE','LAST_TIER_CHANGE_DATE', 'BIRTH_DATE', 'ACTIVITY_DATE', 'FLIGHT_DATE', 'BOOKING_DATE', 'TICKET_SALES_DATE']
+    for col in date_cols: 
+        print(f"process {col}")
+        df[col] = datelib.date_to_timestamp(df[col])
+
+    df.iloc[0].T.head(50)
+    return df
+#-----------------------------------------------------------
+def type_distribution(dff, cols=None):
+    """
+    List the various categories where column names include the word "TYPE", along with countss
+    
+    Arguments
+    ---------
+        dff (DataFrame)
+    Return
+    ------
+        no return
+    """
+    if not isinstance(cols, list):
+        cols = ['ACTIVITY_TYPE','TRANSACTION_TYPE','ISSUING_COMPANY_TYPE','OPERATING_COMPANY_TYPE','SALES_CHANNEL_TYPE']
+
+    for col in cols:
+        a = dff.groupby(col).size()
+        print("-----------------------------------")
+        print("nb classes: ", len(a))
+        print(a[0:20])
+#-----------------------------------------------------------
+def location_stats(df, col_name):
+    """
+    Print out the number of entities encoded in col_name, and return thist list. 
+    Entities are either airports (O/D) or regions or countries. 
+    """
+    col = df[col_name]
+    if rex.findall(r'(region|country)', col_name.lower()):
+        O = set(col)
+        D = set([])
+    else:
+        O = set(col.str[0:3])
+        D = set(col.str[4:7])
+    airports = list(O.union(D)) #.sort()
+    print(f"{col_name}, nb airports: ", len(airports))
+    airports.sort()
+    return airports
+#-----------------------------------------------------------
+def PNRs_with_multiple_MEMBER_IDs(df):
+    """
+    Ensure 1-1 correspondance between PNR and MEMBER_IDs
+
+    Arguments
+    ---------
+    df (Pandas DataFrame)
+        Should have the columns PNR, MEMBER_ID
+
+    Return
+    ------
+    (Pandas DataFrame)
+        Each PNR is associated with a single MEMBER_ID
+    """
+    df_members_g = df[['PNR','MEMBER_ID']].groupby(['PNR','MEMBER_ID'])
+    df_members_g_df = df_members_g.count().reset_index('MEMBER_ID')
+
+    df_members_g_df_g = df_members_g_df.groupby('PNR')
+    size_df = df_members_g_df_g.size().sort_values(ascending=False).to_frame('size')
+
+    PNRsGT1 = size_df[size_df['size'] > 1]
+    PNRsEQ1 = size_df[size_df['size'] == 1]
+
+    print(f"nb PNRs with with more than one member: {PNRsGT1.sum()}")
+    #print(PNRsGT1)
+
+    # Only keep rows with PNR in the list PNRsEQ1
+    return df.set_index('PNR').loc[PNRsEQ1.index].reset_index('PNR')
+#-----------------------------------------------------------
+def read_activity_file(nrows=None): 
+    return pd.read_csv('MEMBERS_ACTIVITY.csv', nrows=nrows, encoding = "ISO-8859-1")
+#-----------------------------------------------------------
+def find_word(lst, word): 
+    lst = list(lst)
+    for l in lst:
+        if word in l:
+            print(l)
+    print()
+#-----------------------------------------------------------
+def keep_depart_country_is_addr_country(df):
+    addr = df['ADDR_COUNTRY'].str.lower().str.strip() == df['TRUE_ORIGIN_COUNTRY'].str.lower().str.strip()
+    return (df[addr])
+#-----------------------------------------------------------
+def seg_and_true_od_dest_match(df):
+    return df[df['TRUE_OD'].str[4:7] == df['SEGMENT_OD'].str[4:7]]
+#-----------------------------------------------------------
+def keep_only(df, col, value):
+    return df[df[col] == value] 
+#-----------------------------------------------------------
+def remove_flights_within_country(df):
+    return df[df['TRUE_ORIGIN_COUNTRY'] != df['TRUE_DESTINATION_COUNTRY']]
+#-----------------------------------------------------------
+def one_record_per_family(df):
+    """
+    Filter the input dataframe to leave only a single ticket per itinerary.
+    Every combination of (PNR / FLIGHT_DATE / OPERATING_FLIGHT_NUMBER) should appear once.
+    If there were multiple tickets associated with this entry, that would be the number
+    of passengers in the party. 
+
+    Arguments
+    ---------
+    df (Pandas DataFrame)
+
+    Return
+    ------
+    Filtered Pandas Dataframe, different from the input. 
+
+    Note
+    ----
+    I am not sure this method is constructed properly. TO DEBUG. 
+    """
+    cols_drop = ['PNR','FLIGHT_DATE', 'OPERATING_FLIGHT_NUMBER']
+    return df.drop_duplicates(subset=cols_drop, keep='first')
+#-----------------------------------------------------------
+def party_size(df):
+    """
+    Identify the size of each party. Group by (PNR / FLIGHT_DATE / OPERATING_FLIGHT_NUMBER / TICKET_NUMBER) 
+    The size of each entry is the party size. 
+
+    Arguments
+    ---------
+    df (Pandas DataFrame)
+
+    Return
+    ------
+    Filtered Pandas Dataframe, different from the input. 
+
+    Note
+    ----
+    I am not sure this method is constructed properly. TO DEBUG. 
+    """
+    cols_party = ['PNR','FLIGHT_DATE', 'OPERATING_FLIGHT_NUMBER', 'TICKET_NUMBER']
+    dfg = df.groupby(cols_party)
+    party_sz = dfg.size()
+    max_party_sz = party_sz.max()
+    print("max party siz: ", max_party_sz)
+    df['PARTY_SZ'] = dfg['TRUE_OD'].transform('size')
+    return df
+#-----------------------------------------------------------
